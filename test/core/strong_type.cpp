74e4746cf302339362e59c7a87d45495e386cc4d
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-12-17 17:12:36 +0100

    cmake: setup doctest

---
+++ test/core/strong_type.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2021-present, Trail of Bits, Inc.
+
+#include <compare> // for operator<, strong_ordering
+#include <doctest/doctest.h>
+#include <gap/core/strong_type.hpp> // for strong_type
+
+namespace gap::test
+{
+    struct int_tag {};
+    using tagged_int = gap::strong_type< int, int_tag >;
+
+    static_assert(tagged_int(1) < tagged_int(2));
+    static_assert(tagged_int(1) == tagged_int(1));
+    static_assert(tagged_int(2) > tagged_int(0));
+
+    TEST_CASE("tagged int") {
+        tagged_int a(1), b(1);
+        CHECK(a == b);
+    }
+
+    struct simple_tag {};
+    struct empty_struct {
+        friend constexpr auto operator<=>(const empty_struct&, const empty_struct&) = default;
+    };
+
+    using tagged_struct = gap::strong_type< empty_struct, simple_tag >;
+
+    static_assert(tagged_struct() == tagged_struct());
+
+} // namespace gap::test
