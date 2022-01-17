e05f2e88b9ed0a471ae9d457af5d13b01ab1c993
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-01-17 16:12:35 +0100

    core: add coroutines example

---
+++ example/coroutines-example.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2022-present, Trail of Bits, Inc.
+
+#include <gap/core/generator.hpp>
+#include <iostream>
+
+gap::generator< int > iota() {
+    int value = 0;
+    while (true) {
+        co_yield value++;
+    }
+}
+
+int main() {
+    for (int v : iota()) {
+        if (v > 5)
+            break;
+        std::cout << v << '\n';
+    }
+}
