18e9deacdca3a4b70664b5c6c373fd92efcadc04
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-01-19 14:53:42 +0100

    test: add simple range tests

---
+++ test/core/ranges.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2022-present, Trail of Bits, Inc.
+
+#include <array>
+#include <doctest/doctest.h>
+#include <gap/core/ranges.hpp>
+#include <vector>
+
+namespace gap::test
+{
+    namespace gr = gap::ranges;
+
+    struct dummy {};
+
+    static_assert(gr::range< std::vector< int > >);
+    static_assert(gr::range< std::vector< dummy > >);
+    static_assert(gr::range< std::array< int, 10 > >);
+    static_assert(gr::range< int > == false);
+
+} // namespace gap::test
