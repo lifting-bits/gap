e4cb1183557a5dfb6afbf8d981e6d2b5a1e81e6c
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-01-23 14:07:10 +0100

    cmake: update and export project settings

--- cmake/project_settings.cmake
+++ cmake/project_settings.cmake
@@ -1,82 +1,40 @@
-# Copyright (c) 2021-present, Trail of Bits, Inc. All rights reserved.
+# Copyright (c) 2022-present, Trail of Bits, Inc. All rights reserved.
 
-# This is only executed once; use a macro (and not a function) so that everything defined here does
-# not end up in a separate namespace
-macro (SETTINGS_MAIN)
-  # default build type
-  if (WIN32)
-    set(CMAKE_BUILD_TYPE Release)
-  else ()
-    # Set a default build type if none was specified
-    if (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-      message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
-      set(CMAKE_BUILD_TYPE
-          RelWithDebInfo
-          CACHE STRING "Choose the type of build." FORCE
-      )
-      # Set the possible values of build type for cmake-gui, ccmake
-      set_property(
-        CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo"
-      )
-    endif ()
-  endif ()
+# generate a compile commands JSON file.
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-  # overwrite the default install prefix
-  if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-    if (DEFINED WIN32)
-      set(CMAKE_INSTALL_PREFIX "C:/")
-    else ()
-      set(CMAKE_INSTALL_PREFIX "/usr/local")
-    endif ()
-  endif ()
+# Set a default build type if none was specified
+if( NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES )
+  message( STATUS "Setting build type to 'RelWithDebInfo' as none was specified." )
+  set(CMAKE_BUILD_TYPE
+      RelWithDebInfo
+      CACHE STRING "Choose the type of build." FORCE)
+  # Set the possible values of build type for cmake-gui, ccmake
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
+                                               "MinSizeRel" "RelWithDebInfo")
+endif()
 
-  message(STATUS "Install prefix: ${CMAKE_INSTALL_PREFIX}")
+find_program( CCACHE ccache )
+if ( CCACHE )
+  message( STATUS "Using ccache" )
+  set( CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE} )
+else()
+  message( WARNING "CCache was not found." )
+endif()
 
-  # generate a compile commands JSON file.
-  set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+# Generate compile_commands.json to make it easier to work with clang based tools
+set( CMAKE_EXPORT_COMPILE_COMMANDS ON )
 
-  #
-  # compiler and linker flags
-  #
-  option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" OFF)
+option( ENABLE_IPO
+  "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" OFF
+)
 
-  if (ENABLE_IPO)
-    include(CheckIPOSupported)
-    # cmake-lint: disable=E1122
-    check_ipo_supported(
-      RESULT
-      result
-      OUTPUT
-      output
-    )
-    if (result)
-      set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
-    else ()
-      message(WARNING "IPO is not supported: ${output}")
-    endif ()
-  endif ()
-
-  # Globally set the required C++ standard
-  set(CMAKE_CXX_STANDARD 20)
-  set(CMAKE_CXX_EXTENSIONS OFF)
-
-  if (UNIX)
-    if (APPLE)
-      set(PLATFORM_NAME "macos")
-    else ()
-      set(PLATFORM_NAME "linux")
-    endif ()
-
-  elseif (WIN32)
-    set(PLATFORM_NAME "windows")
-
-  else ()
-    message("This platform is not officially supported")
-  endif ()
-
-  set(SETTINGS_CMAKE_ true)
-endmacro ()
-
-if (NOT DEFINED SETTINGS_CMAKE_)
-  settings_main()
-endif ()
+if( ENABLE_IPO )
+  include( CheckIPOSupported )
+  check_ipo_supported( RESULT res OUTPUT out )
+  if( res )
+    set( CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE )
+  else()
+    message( SEND_ERROR "IPO is not supported: ${out}" )
+  endif()
+endif()
