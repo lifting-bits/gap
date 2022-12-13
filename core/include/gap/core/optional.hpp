4526dd22d4b9b0843f043942b0e1ba9ffa8a762b
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-13 16:56:28 +0100

    core: add and_then for pointer like optionals

--- core/include/gap/core/optional.hpp
+++ core/include/gap/core/optional.hpp
@@ -153,7 +153,6 @@ namespace gap {
         const base&& unwrap() const&& { return std::move(*this); }
 
         template< typename F, optional_like R = std::invoke_result_t< F, value_type& > >
-        requires(!std::is_pointer_v< base >)
         constexpr auto and_then( F&& f ) & {
             if (*this) {
                 return std::invoke(std::forward< F >(f), this->value());
@@ -163,7 +162,6 @@ namespace gap {
         }
 
         template< typename F, optional_like R = std::invoke_result_t< F, const value_type& > >
-        requires(!std::is_pointer_v< base >)
         constexpr auto and_then( F&& f ) const& {
             if (*this) {
                 return std::invoke(std::forward< F >(f), this->value());
@@ -173,7 +171,6 @@ namespace gap {
         }
 
         template< typename F, optional_like R = std::invoke_result_t< F, value_type > >
-        requires(!std::is_pointer_v< base >)
         constexpr auto and_then( F&& f ) && {
             if (*this) {
                 return std::invoke(std::forward< F >(f), std::move(this->value()));
@@ -183,7 +180,6 @@ namespace gap {
         }
 
         template< typename F, optional_like R = std::invoke_result_t< F, value_type > >
-        requires(!std::is_pointer_v< base >)
         constexpr auto and_then( F&& f ) const&& {
             if (*this) {
                 return std::invoke(std::forward< F >(f), std::move(this->value()));
