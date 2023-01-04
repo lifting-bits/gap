04207a0d6421678c7fc1721822844aab8569eaae
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2023-01-04 14:51:17 +0100

    core: no implicit `stream`, universal references

--- core/include/gap/core/functional.hpp
+++ core/include/gap/core/functional.hpp
@@ -4,6 +4,7 @@
 
 #ifdef GAP_ENABLE_COROUTINES
 
+    #include "gap/core/concepts.hpp"
     #include "gap/core/generator.hpp"
 
     #include <optional>
@@ -12,46 +13,44 @@ namespace gap
 {
     namespace functional
     {
-        auto stream(auto&& cont) -> gap::generator< decltype(*cont.begin()) > {
+        template< typename T >
+        requires iterable< T >
+        auto stream(T&& cont) -> gap::generator< decltype(*cont.begin()) > {
             for (auto&& elem : cont) {
                 co_yield std::forward< decltype(elem) >(elem);
             }
         }
 
         template< typename T >
-        gap::generator< T > stream(gap::generator< T > gen) {
-            return gen;
-        }
-
-        template< typename T >
-        auto stream(const std::optional< T >& cont) -> gap::generator< T > {
-            if (cont.has_value()) {
-                auto value = cont.value();
-                co_yield value;
+        requires std::convertible_to< T, bool > && dereferenceable< T >
+        auto stream(T&& cont) -> gap::generator< decltype(*cont) > {
+            if (cont) {
+                co_yield std::forward< decltype(*cont) >(*cont);
             }
         }
 
-        template< typename TFunc, typename TSource >
-        auto map(TFunc&& f, TSource&& source)
-            -> gap::generator< decltype(f(*stream(source).begin())) > {
-            for (auto&& elem : stream(std::forward< decltype(source) >(source))) {
+        template< typename TFunc, typename TInput >
+        auto map(TFunc&& f, gap::generator< TInput > source)
+            -> gap::generator< decltype(f(*source.begin())) > {
+            for (auto&& elem : source) {
                 co_yield f(std::forward< decltype(elem) >(elem));
             }
         }
 
-        template< typename TPred, typename TSource >
-        auto filter(TPred&& pred, TSource&& source) -> decltype(stream(source)) {
-            for (auto&& elem : stream(std::forward< decltype(source) >(source))) {
+        template< typename TPred, typename TElem >
+        gap::generator< TElem > filter(TPred&& pred, gap::generator< TElem > source) {
+            for (auto&& elem : source) {
                 if (pred(std::forward< decltype(elem) >(elem))) {
                     co_yield std::forward< decltype(elem) >(elem);
                 }
             }
         }
 
-        template< typename TFunc, typename TSource >
-        auto flat_map(TFunc&& f, TSource&& source) -> decltype(stream(f(*stream(source).begin()))) {
-            for (auto&& elem : stream(std::forward< decltype(source) >(source))) {
-                for (auto&& sub_elem : stream(f(std::forward< decltype(elem) >(elem)))) {
+        template< typename TFunc, typename TElem >
+        auto flat_map(TFunc&& f, gap::generator< TElem > source)
+            -> gap::generator< decltype(*(f(*source.begin()).begin())) > {
+            for (auto&& elem : source) {
+                for (auto&& sub_elem : f(source)) {
                     co_yield std::forward< decltype(sub_elem) >(sub_elem);
                 }
             }
--- test/core/functional.cpp
+++ test/core/functional.cpp
@@ -14,13 +14,15 @@ namespace gap::test
 {
     TEST_CASE("map vector") {
         const std::vector< int > vec{ 1, 2, 3, 4, 5, 6 };
-        auto gen = gap::functional::map([](int x) { return std::make_pair(x, x * 2); }, vec);
+        auto gen = gap::functional::map(
+            [](int x) { return std::make_pair(x, x * 2); }, gap::functional::stream(vec));
         for (auto [a, b] : gen) {
             CHECK_EQ(2 * a, b);
         }
 
         gen = gap::functional::map(
-            [](int x) { return std::make_pair(x, x * 2); }, std::vector< int >{ 1, 2, 3, 4, 5, 6 });
+            [](int x) { return std::make_pair(x, x * 2); },
+            gap::functional::stream(std::vector< int >{ 1, 2, 3, 4, 5, 6 }));
         for (auto [a, b] : gen) {
             CHECK_EQ(2 * a, b);
         }
@@ -28,13 +30,15 @@ namespace gap::test
 
     TEST_CASE("filter vector") {
         const std::vector< int > vec{ 1, 2, 3, 4, 5, 6 };
-        auto gen = gap::functional::filter([](int x) { return x % 2; }, vec);
+        auto gen
+            = gap::functional::filter([](int x) { return x % 2; }, gap::functional::stream(vec));
         for (auto x : gen) {
             CHECK(x % 2);
         }
 
         auto new_gen = gap::functional::filter(
-            [](int x) { return x % 2; }, std::vector< int >{ 1, 2, 3, 4, 5, 6 });
+            [](int x) { return x % 2; },
+            gap::functional::stream(std::vector< int >{ 1, 2, 3, 4, 5, 6 }));
         for (auto x : new_gen) {
             CHECK(x % 2);
         }
@@ -48,26 +52,13 @@ namespace gap::test
                     co_yield x;
                 }
             },
-            vec);
-        std::vector< int > res;
-        gap::functional::insert_into(std::back_inserter(res), gen);
-        CHECK_EQ(res.size(), 6);
-    }
-
-    TEST_CASE("flatmap optional") {
-        const std::vector< int > vec{ 1, 2, 3 };
-        auto gen = gap::functional::flat_map(
-            [](int x) -> std::optional< int > {
-                if (x % 2 == 0) {
-                    return x;
-                } else {
-                    return std::nullopt;
-                }
-            },
-            vec);
-        for (auto x : gen) {
-            CHECK_EQ(x, 2);
+            gap::functional::stream(vec));
+        int count = 0;
+        for (auto elem : gen) {
+            (void) elem;
+            ++count;
         }
+        CHECK_EQ(count, 6);
     }
 
 } // namespace gap::test
