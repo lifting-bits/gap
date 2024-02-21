bfb9ed0d10c3dc4f0dbd3fcba08b1d76b269ea05
xlauko <xlauko@mail.muni.cz>
2024-02-21 12:51:05 +0100

    cmake: add library packaging utils

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -14,5 +14,31 @@ function(add_gap_library name headers)
 	set_target_properties(${name} PROPERTIES PUBLIC_HEADER "${headers}")
 
 	target_link_libraries(${name} INTERFACE gap-settings)
+endfunction()
 
-endfunction(add_gap_lib)
+function(install_gap_target name include_dir)
+	set(GAP_EXPORT_NAME gapTargets)
+
+	install(TARGETS ${name}
+		EXPORT ${GAP_EXPORT_NAME}
+		RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+		LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+			NAMELINK_SKIP
+		ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+		PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/gap/${include_dir}
+	)
+
+	install(EXPORT ${GAP_EXPORT_NAME}
+		FILE ${GAP_EXPORT_NAME}.cmake
+		NAMESPACE gap::
+		DESTINATION ${GAP_CMAKE_INSTALL_DIR}
+	)
+
+	install(TARGETS ${name}
+		RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+		LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+			NAMELINK_ONLY
+		ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+		PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/gap/${include_dir}
+	)
+endfunction()
