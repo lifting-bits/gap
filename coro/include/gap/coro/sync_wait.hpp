9c373b52f0a008d5a3379c4cbf21f28845e1e56a
xlauko <xlauko@mail.muni.cz>
2024-02-27 11:00:28 +0100

    coro: unify sync wait

--- coro/include/gap/coro/sync_wait.hpp
+++ coro/include/gap/coro/sync_wait.hpp
@@ -6,137 +6,181 @@
 
     #include <gap/coro/awaitable_traits.hpp>
     #include <gap/coro/coroutine.hpp>
+    #include <gap/coro/manual_reset_event.hpp>
 
     #include <semaphore>
     #include <cassert>
 
 namespace gap::coro
 {
-    template< awaitable awaitable_t >
-    auto sync_wait(awaitable_t &&awaitable) -> await_result_t< awaitable_t > {
-        struct void_type {};
+    template< typename result_t >
+    struct sync_wait_task;
 
-        using result_t = await_result_t< awaitable_t >;
-        using storage_type = std::add_pointer_t<
-            std::conditional_t<
-                std::is_void_v< result_t >,
-                void_type, result_t
-            >
-        >;
-        using result_type = std::variant< std::monostate, storage_type, std::exception_ptr >;
+    template< typename result_t >
+    struct sync_wait_task_promise {
+        using coroutine_handle = gap::coroutine_handle< sync_wait_task_promise >;
 
-        struct sync_wait_task {
-            struct promise_type {
-                using coroutine_handle = gap::coroutine_handle< promise_type >;
+        sync_wait_task_promise() noexcept = default;
 
-                sync_wait_task get_return_object() noexcept {
-                    return sync_wait_task{ coroutine_handle::from_promise(*this) };
+        void start(manual_reset_event &event) noexcept {
+            m_event = &event;
+            coroutine_handle::from_promise(*this).resume();
+        }
+
+        auto get_return_object() noexcept {
+            return coroutine_handle::from_promise(*this);
+        }
+
+        gap::suspend_always initial_suspend() noexcept {
+            return {};
+        }
+
+        auto final_suspend() noexcept {
+            struct final_awaiter {
+                bool await_ready() const noexcept {
+                    return false;
                 }
 
-                gap::suspend_always initial_suspend() const noexcept { return {}; }
-
-                struct final_awaiter {
-                    bool await_ready() const noexcept { return false; }
-
-                    void await_suspend(coroutine_handle coroutine) noexcept {
-                        // Now that coroutine has suspended we can signal the semaphore,
-                        // unblocking the waiting thread. The other thread will then
-                        // destroy this coroutine (which is safe because it is suspended).
-                        coroutine.promise().m_semaphore.release();
-                    }
-
-                    void await_resume() const noexcept {}
-                };
-
-                final_awaiter final_suspend() const noexcept { return {}; }
-
-                using non_void_return_type = std::conditional_t< std::is_void_v<result_t>, void_type, result_t >;
-
-                final_awaiter yield_value(non_void_return_type value) requires (!std::is_void_v< result_t >) {
-                    // Note that we just store the address here and then suspend
-                    // and unblock the waiting thread which then copies/moves the
-                    // result from this address directly to the return-value of
-                    // sync_wait(). This avoids having to make an extra intermediate
-                    // copy of the result value.
-                    m_result.template emplace< storage_type >(std::addressof(value));
-                    return {};
+                void await_suspend(coroutine_handle coroutine) noexcept {
+                    coroutine.promise().m_event->set();
                 }
 
-                void return_void() noexcept {
-                    m_result.template emplace< storage_type >();
-                }
-
-                void unhandled_exception() noexcept {
-                    m_result.template emplace< std::exception_ptr >(std::current_exception());
-                }
-
-                std::binary_semaphore m_semaphore{0};
-                result_type m_result;
+                void await_resume() const noexcept {}
             };
 
-            using coroutine_handle = gap::coroutine_handle< promise_type >;
+            return final_awaiter{};
+        }
 
-            explicit sync_wait_task(coroutine_handle coroutine) noexcept
-                : m_coroutine(coroutine) {}
+        auto yield_value(result_t &&value) noexcept {
+            m_result = std::addressof(value);
+            return final_suspend();
+        }
 
-            sync_wait_task(const sync_wait_task &) = delete;
-            sync_wait_task(sync_wait_task &&other) noexcept
-                : m_coroutine(std::exchange(other.m_coroutine, {})) {}
+        void return_void() noexcept {
+            assert(false);
+        }
 
-            ~sync_wait_task() {
-                if (m_coroutine) {
-                    m_coroutine.destroy();
-                }
+        void unhandled_exception() noexcept {
+            m_exception = std::current_exception();
+        }
+
+        result_t &&result() {
+            if (m_exception) {
+                std::rethrow_exception(m_exception);
             }
 
-            sync_wait_task &operator=(const sync_wait_task &) = delete;
-            sync_wait_task &operator=(sync_wait_task &&other) {
-                if (this != std::addressof(other)) {
-                    if (m_coroutine) {
-                        m_coroutine.destroy();
-                    }
+            return static_cast< result_t&& >(*m_result);
+        }
+        private:
+        manual_reset_event *m_event = nullptr;
+        std::remove_reference_t< result_t > *m_result = nullptr;
+        std::exception_ptr m_exception;
+    };
 
-                    m_coroutine = std::exchange(other.m_coroutine, {});
+    template<>
+    struct sync_wait_task_promise< void >
+    {
+        using coroutine_handle = gap::coroutine_handle< sync_wait_task_promise >;
+
+        sync_wait_task_promise() noexcept = default;
+
+        void start(manual_reset_event &event) noexcept {
+            m_event = &event;
+            coroutine_handle::from_promise(*this).resume();
+        }
+
+        auto get_return_object() noexcept {
+            return coroutine_handle::from_promise(*this);
+        }
+
+        gap::suspend_always initial_suspend() noexcept {
+            return {};
+        }
+
+        auto final_suspend() noexcept {
+            struct final_awaiter {
+                bool await_ready() const noexcept {
+                    return false;
                 }
 
-                return *this;
-            }
-
-            // The force-inline here is required to get the _sync_task coroutine
-            // elided.  Otherwise the compiler doesn't know that this function
-            // hasn't modified 'coro' member variable and so can't deduce that it's
-            // always destroyed before sync_wait() returns.
-            __attribute__((always_inline)) result_t run() {
-                m_coroutine.resume();
-                m_coroutine.promise().m_semaphore.acquire();
-                auto& result = m_coroutine.promise().m_result;
-
-                if (result.index() == 2) {
-                    std::rethrow_exception(std::get< 2 >(result));
+                void await_suspend(coroutine_handle coroutine) noexcept {
+                    coroutine.promise().m_event->set();
                 }
 
-                assert(result.index() == 1);
+                void await_resume() const noexcept {}
+            };
 
-                if constexpr (std::is_void_v< result_t >) {
-                    return;
-                } else {
-                    return static_cast< result_t&& >(*std::get< 1 >(result));
-                }
+            return final_awaiter{};
+        }
+
+        void return_void() noexcept {}
+
+        void unhandled_exception() noexcept {
+            m_exception = std::current_exception();
+        }
+
+        void result() {
+            if (m_exception) {
+                std::rethrow_exception(m_exception);
             }
+        }
 
-            private:
-                coroutine_handle m_coroutine;
-        };
+      private:
+        manual_reset_event *m_event = nullptr;
+        std::exception_ptr m_exception;
+    };
 
-        return [&] () -> sync_wait_task {
-            if constexpr (std::is_void_v< result_t >) {
-                co_await static_cast< awaitable_t&& >(awaitable);
-            } else {
-                // use co_yield instead of co_return so we suspend while the
-                // potentially temporary result of co_await is still alive.
-                co_yield co_await static_cast< awaitable_t&& >(awaitable);
-            }
-        }().run();
+    template< typename result_t >
+    struct sync_wait_task final
+    {
+        using promise_type = sync_wait_task_promise< result_t >;
+        using coroutine_handle = gap::coroutine_handle< promise_type >;
+
+        sync_wait_task(coroutine_handle coroutine) noexcept
+            : m_coroutine(coroutine)
+        {}
+
+        sync_wait_task(sync_wait_task const &other) = delete;
+        sync_wait_task(sync_wait_task &&other) noexcept
+            : m_coroutine(std::exchange(other.m_coroutine, coroutine_handle{}))
+        {}
+
+        sync_wait_task &operator=(sync_wait_task const &other) = delete;
+        sync_wait_task &operator=(sync_wait_task &&other) noexcept {
+            m_coroutine = std::exchange(other.m_coroutine, coroutine_handle{});
+            return *this;
+        }
+
+        void start(manual_reset_event &event) noexcept {
+            m_coroutine.promise().start(event);
+        }
+
+        decltype(auto) result() {
+            return m_coroutine.promise().result();
+        }
+      private:
+        coroutine_handle m_coroutine;
+    };
+
+    template< awaitable awaitable_t, typename result_t = await_result_t< awaitable_t&& > >
+    requires (not std::is_void_v< result_t >)
+    sync_wait_task< result_t > make_sync_wait_task(awaitable_t &&awaitable) {
+        co_yield co_await std::forward< awaitable_t >(awaitable);
+    }
+
+    template< awaitable awaitable_t, typename result_t = await_result_t< awaitable_t&& > >
+    requires std::is_void_v< result_t >
+    sync_wait_task< void > make_sync_wait_task(awaitable_t &&awaitable) {
+        co_await std::forward< awaitable_t >(awaitable);
+    }
+
+    template< awaitable awaitable_t >
+    auto sync_wait(awaitable_t &&awaitable) -> await_result_t< awaitable_t > {
+        auto task = make_sync_wait_task(std::forward< awaitable_t >(awaitable));
+        manual_reset_event event;
+        task.start(event);
+        event.wait();
+        return task.result();
     }
 } // namespace gap::coro
 
