bb3affab1a736c5cd229137fd88b5622f996d251
xlauko <xlauko@mail.muni.cz>
2024-03-01 20:12:57 +0000

    test: extract counted class

--- test/coro/task.cpp
+++ test/coro/task.cpp
@@ -18,49 +18,12 @@
     #include <gap/coro/task.hpp>
     #include <gap/coro/when_all_ready.hpp>
 
+    #include "counted.hpp"
+
 using namespace gap::coro;
 
 namespace gap::test
 {
-    struct counted {
-        static int default_construction_count;
-        static int copy_construction_count;
-        static int move_construction_count;
-        static int destruction_count;
-
-        int id;
-
-        static void reset_counts() {
-            default_construction_count = 0;
-            copy_construction_count    = 0;
-            move_construction_count    = 0;
-            destruction_count          = 0;
-        }
-
-        static int construction_count() {
-            return default_construction_count + copy_construction_count + move_construction_count;
-        }
-
-        static int active_count() { return construction_count() - destruction_count; }
-
-        counted()
-            : id(default_construction_count++) {}
-        counted(const counted& other)
-            : id(other.id) {
-            ++copy_construction_count;
-        }
-        counted(counted&& other)
-            : id(other.id) {
-            ++move_construction_count;
-            other.id = -1;
-        }
-        ~counted() { ++destruction_count; }
-    };
-
-    int counted::default_construction_count;
-    int counted::copy_construction_count;
-    int counted::move_construction_count;
-    int counted::destruction_count;
 
     TEST_SUITE_BEGIN("task");
 
