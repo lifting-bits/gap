2a4bade0fecdbf3008caee531c903a6f6ae715f4
xlauko <xlauko@mail.muni.cz>
2024-03-01 17:53:31 +0000

    cmake: make coro static library

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -16,6 +16,24 @@ function(add_gap_library name headers)
 	target_link_libraries(${name} INTERFACE gap-settings)
 endfunction()
 
+function(add_gap_static_library name headers sources)
+	add_library(${name} STATIC ${sources})
+
+	add_library(gap::${name} ALIAS ${name})
+
+	target_compile_features(${name} INTERFACE cxx_std_20)
+
+	target_include_directories(${name}
+		PUBLIC
+			$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+	    	$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
+	)
+
+	set_target_properties(${name} PROPERTIES PUBLIC_HEADER "${headers}")
+
+	target_link_libraries(${name} INTERFACE gap-settings)
+endfunction()
+
 function(install_gap_target name include_dir)
 	set(GAP_EXPORT_NAME gapTargets)
 
