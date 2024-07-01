ff277b6fe9f82075f88f0da5d65bf19bc79c6ee5
xlauko <xlauko@mail.muni.cz>
2024-07-01 20:55:59 +0200

    mlir: Add casted views.

--- mlir/include/gap/mlir/views.hpp
+++ mlir/include/gap/mlir/views.hpp
@@ -19,10 +19,24 @@
     #endif
 #endif
 
+#include <gap/mlir/functors.hpp>
+
 namespace gap::mlir::views
 {
     auto regions(operation op) -> decltype(std::views::all(op->getRegions()));
     auto blocks(operation op) -> decltype(std::views::join(regions(op)));
     auto operations(operation op) -> decltype(std::views::join(blocks(op)));
+
+    template< typename T >
+    auto isa = std::views::filter(::gap::mlir::isa< T >);
+
+    template< typename T >
+    auto cast = std::views::transform(::gap::mlir::cast< T >);
+
+    template< typename T >
+    auto dyn_cast = std::views::transform(::gap::mlir::dyn_cast< T >);
+
+    template< typename T >
+    auto filter_cast = isa< T > | cast< T >;
 
 } // namespace gap::mlir::views
