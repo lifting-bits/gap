4e9c095030809cc4636f6435ef7aa0ebd1bf94f0
xlauko <xlauko@mail.muni.cz>
2024-07-01 18:19:44 +0200

    mlir: Add mlir common usings.

---
+++ mlir/include/gap/mlir/common.hpp
@@ -0,0 +1,25 @@
+// Copyright 2024, Trail of Bits, Inc.
+
+#pragma once
+
+#include <mlir/IR/Attributes.h>
+#include <mlir/IR/Block.h>
+#include <mlir/IR/Location.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/Operation.h>
+#include <mlir/IR/Region.h>
+#include <mlir/IR/TypeRange.h>
+#include <mlir/IR/Value.h>
+
+namespace gap::mlir
+{
+    using attr       = ::mlir::Attribute;
+    using type       = ::mlir::Type;
+    using type_range = ::mlir::TypeRange;
+    using value      = ::mlir::Value;
+    using block      = ::mlir::Block;
+    using region     = ::mlir::Region;
+    using operation  = ::mlir::Operation *;
+    using context    = ::mlir::MLIRContext;
+    using location   = ::mlir::Location;
+} // namespace gap::mlir
