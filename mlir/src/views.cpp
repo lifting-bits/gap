db0d04c6cf4857f727a348bc6f682454c3524d4d
xlauko <xlauko@mail.muni.cz>
2024-07-01 19:58:24 +0200

    mlir: Setup basic operation views.

---
+++ mlir/src/views.cpp
@@ -0,0 +1,17 @@
+#include <gap/mlir/views.hpp>
+
+namespace gap::mlir
+{
+    auto regions(operation op) -> decltype(std::views::all(op->getRegions())) {
+        return op->getRegions() | std::views::all;
+    }
+
+    auto blocks(operation op) -> decltype(std::views::join(regions(op))) {
+        return regions(op) | std::views::join;
+    }
+
+    auto operations(operation op) -> decltype(std::views::join(blocks(op))) {
+        return blocks(op) | std::views::join;
+    }
+
+} // namespace gap::mlir
