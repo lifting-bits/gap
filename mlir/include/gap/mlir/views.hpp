471324ebb06a17494e47f1bb0fc74e811145a632
xlauko <xlauko@mail.muni.cz>
2024-08-15 12:29:03 +0000

    mlir: fix regions view

--- mlir/include/gap/mlir/views.hpp
+++ mlir/include/gap/mlir/views.hpp
@@ -23,9 +23,18 @@
 
 namespace gap::mlir::views
 {
-    auto regions(operation op) -> decltype(std::views::all(op->getRegions()));
-    auto blocks(operation op) -> decltype(std::views::join(regions(op)));
-    auto operations(operation op) -> decltype(std::views::join(blocks(op)));
+    static inline auto regions(operation op) -> decltype(auto) {
+        auto regs = op->getRegions();
+        return std::ranges::subrange{regs.begin(), regs.end()};
+    }
+
+    static inline auto blocks(operation op) -> decltype(auto) {
+        return regions(op) | std::views::join;
+    }
+
+    static inline auto operations(operation op) -> decltype(auto) {
+        return blocks(op) | std::views::join;
+    }
 
     template< typename T >
     auto isa = std::views::filter(::gap::mlir::isa< T >);
