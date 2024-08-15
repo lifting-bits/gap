53b1a07b1f80036774c747266c23fd703c91228c
xlauko <xlauko@mail.muni.cz>
2024-08-15 12:29:03 +0000

    mlir: fix regions view

--- mlir/src/views.cpp
+++ mlir/src/views.cpp
@@ -2,16 +2,5 @@
 
 namespace gap::mlir::views
 {
-    auto regions(operation op) -> decltype(std::views::all(op->getRegions())) {
-        return op->getRegions() | std::views::all;
-    }
-
-    auto blocks(operation op) -> decltype(std::views::join(regions(op))) {
-        return regions(op) | std::views::join;
-    }
-
-    auto operations(operation op) -> decltype(std::views::join(blocks(op))) {
-        return blocks(op) | std::views::join;
-    }
 
 } // namespace gap::mlir::views
